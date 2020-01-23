f9857748452fac62ce3bbe10074ad6342a0d4547
Rodrigo Castro <rodrigo.castro@easynvest.com.br>
2020-01-23 16:34:26 -0300

    fetch data from firebase

--- lib/band_list_screen.dart
+++ lib/band_list_screen.dart
@@ -1,5 +1,5 @@
-import 'package:bandnames/band.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BandListScreen extends StatefulWidget {
   BandListScreen({Key key, this.title}) : super(key: key);
@@ -11,30 +11,29 @@ class BandListScreen extends StatefulWidget {
 }
 
 class _BandListScreenState extends State<BandListScreen> {
-  static const List<Band> _bandList = [
-    Band(name: 'Metallica', votes: 1),
-    Band(name: 'Fall Out Boy', votes: 2),
-    Band(name: 'Offspring', votes: 3),
-    Band(name: 'Gorillaz', votes: 4),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: ListView.builder(
-            itemCount: _bandList.length,
-            itemBuilder: (context, index) =>
-                _buildListItem(context, _bandList[index])));
+        body: StreamBuilder(
+            stream: Firestore.instance.collection('bands').snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const Text('Loading ...');
+              return ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) =>
+                    _buildListItem(context, snapshot.data.documents[index]),
+              );
+            }));
   }
 
-  Widget _buildListItem(context, band) {
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
     return ListTile(
-      title: Text(band.name),
+      title: Text(document['name']),
       trailing: CircleAvatar(
-        child: Text(band.votes.toString()),
+        child: Text(document['votes'].toString()),
       ),
     );
   }
